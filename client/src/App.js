84ee1e855941c530df04505bcc5f77054466f28c
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-10-13 20:37:43 -0700

    added simple login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,49 @@
 import ListHeader from './components/ListHeader'
 import ListItem from './components/ListItem'
+import Auth from './components/Auth'
 import React from "react"
 
 
 function App() {
   const userEmail = 'jimmy@test.com'
   const [tasks, setTasks] = React.useState(null)
+
+  const authToken = false
+
   //get the data from Postgres
   const getData = async () =>{
     try{
       const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
       const data = await response.json()
-      console.log(data)
       setTasks(data)
     }catch(err){
       console.log(err)
     }
   }
 
-  React.useEffect(()=>getData, [])
+  React.useEffect(()=>{
+    if(authToken){
+      getData()
+    }
+  }, [])
 
-  console.log(tasks)
-  
   //Sort by date
   const sortedTasks = tasks?.sort((a,b) => new Date(a.date)- new Date(b.date))
 
   return (
     <div className="app">
-      <ListHeader listName={"Todo List"} getData={getData}  />
-      {sortedTasks?.map((task)=>{
-        return(
-          <ListItem key={task.id} task={task} getData={getData}/>
-        ) 
-      })}
+    {/* If the auth token exist show the app, if not show the login page */}
+      {!authToken&& <Auth/>}
+      {authToken && <>
+        <ListHeader listName={"Todo List"} getData={getData}  />
+        {sortedTasks?.map((task)=>{
+          return(
+            <ListItem key={task.id} task={task} getData={getData}/>
+          ) 
+        })}
 
+      </>}
+      
           
     </div>
   );
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,7 +1,40 @@
-export default function Auth(){
-  return(
-    <>
+import { useState } from "react"
 
-    </>
+
+export default function Auth(){
+  const [isLogIn, setIsLogin] = useState(true)
+  const [error, setError] = useState(null)
+ 
+  const viewLogin = (status) =>{
+    setError(null)
+    setIsLogin(status)
+  }
+
+  return(
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form>
+          {isLogIn ? 'Please log in' : 'please sign up!'}
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password"/>
+          {!isLogIn && <input type="password" placeholder="confirm password"/>}
+          <input type="submit" className="create" />
+          {error && <p>{error}</p>}
+        </form>
+
+        <div className="auth-options">
+          <button 
+            onClick={()=>{viewLogin(false)}}
+            style={{backgroundColor : !isLogIn ? 'rgb(255,255,255)':'rgb(188,188,188)'}}
+          >Sign Up</button>
+          <button 
+            onClick={()=>{viewLogin(true)}}
+            style={{backgroundColor : isLogIn ? 'rgb(255,255,255)':'rgb(188,188,188)'}}
+          >Login</button>
+        </div>
+
+
+      </div>
+    </div>
   )
 }
