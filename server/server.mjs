d97dd835817526d1f7ef6ba5aefdacced5c7c7b4
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-10-23 15:52:15 -0700

    updated static page for the server

--- server/server.mjs
+++ server/server.mjs
@@ -23,7 +23,7 @@ app.use(express.json())
 if(process.env.NODE_ENV === "production"){
   //server static content
   //npm run build
-  app.use(express.static(path.join(__dirname, "client/build")))
+  app.use(express.static(path.join(__dirname, "../client/build")))
 }
 
 
