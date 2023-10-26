0fe9375032629316797592454ff1004a54b66e47
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-10-26 14:22:36 -0700

    going back to the basics

--- server/db.mjs
+++ server/db.mjs
@@ -6,24 +6,16 @@ import dotenv from "dotenv";
 dotenv.config();
 const {Pool} = pg
 
-const devConfig = {
-    user:process.env.USER,
-    password: process.env.PASSWORD,
-    host:process.env.HOST,
-    port:process.env.DBPORT,
-    database: process.env.DATABASE
-};
 
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL 
+const pool = new Pool({
+  user:process.env.USER,
+  password: process.env.PASSWORD,
+  host:process.env.HOST,
+  port:process.env.DBPORT,
+  database: process.env.DATABASE
 }
-
-//heroku provides a node_env to check if its in production or not
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
 )
-
 
 
 export default pool
