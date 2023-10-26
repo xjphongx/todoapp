47a5e3c650e0ee7a3f32e31e9272b1c614b27965
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-10-26 14:34:02 -0700

    updating

--- server/db.mjs
+++ server/db.mjs
@@ -6,16 +6,24 @@ import dotenv from "dotenv";
 dotenv.config();
 const {Pool} = pg
 
+const devConfig = {
+    user:process.env.USER,
+    password: process.env.PASSWORD,
+    host:process.env.HOST,
+    port:process.env.DBPORT,
+    database: process.env.DATABASE
+};
 
 
-const pool = new Pool({
-  user:process.env.USER,
-  password: process.env.PASSWORD,
-  host:process.env.HOST,
-  port:process.env.DBPORT,
-  database: process.env.DATABASE
+const proConfig = {
+  connectionString: process.env.DATABASE_URL 
 }
+
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
 )
+
 
 
 export default pool
