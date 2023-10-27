9040488fc1fd7b3d6a23630061759b40eb56ef36
xjphongx <JimmyPhong16@csu.fullerton.edu>
2023-10-27 00:20:30 -0700

    asdf

--- server/server.mjs
+++ server/server.mjs
@@ -26,7 +26,7 @@ app.use(express.json())
   app.use(express.static(path.join(__dirname, "../client/build")))
 } */
 
-app.get('', (req,res)=>{
+app.get('/', (req,res)=>{
   res.json("hello world")
 })
 
